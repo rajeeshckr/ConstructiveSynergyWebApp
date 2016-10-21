cb9504e8339b2491146355a46c199219b0d09566
Rajeesh <Rajeesh@RAJEESH-PC>
2016-10-21 17:23:59 +1100

    Base mvc project with identity framework

---
+++ ConstructiveSynergyWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ConstructiveSynergyWeb.Startup))]
+namespace ConstructiveSynergyWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
